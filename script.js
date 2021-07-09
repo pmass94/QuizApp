35106b4ea00dc39a2b6d5e64015a9b9a101a50f6
Peter Mass <mass.peter@web.de>
2021-07-09 09:15:16 +0200

    version update

---
+++ script.js
@@ -0,0 +1,33 @@
+let questions = [
+    {
+        "question": "Welches ist die Hauptstadt von Ungarn?",
+        "answer_1": "Kiew",
+        "answer_2": "Bukarest",
+        "answer_3": "Minsk",
+        "answer_4": "Budapest",
+        "right_answer": 4
+
+    },
+    {
+        "question": "Wer schoss das Siegtor im Finale der Fussball WM 2010?",
+        "answer_1": "Arjen Robben",
+        "answer_2": "David Villa",
+        "answer_3": "Andres Iniesta",
+        "answer_4": "Xabi Alonso",
+        "right_answer": 3
+
+    },
+    {
+        "question": "Wie viele Grand-Slam-Titel konnte Novak Djokovic schon gewinnen?",
+        "answer_1": "14",
+        "answer_2": "19",
+        "answer_3": "23",
+        "answer_4": "17",
+        "right_answer":2
+    },
+
+
+
+
+
+];
